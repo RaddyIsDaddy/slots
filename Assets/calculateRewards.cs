16cbdccb2ea855826726edc415b8e6658c5dc60e
RaddyIsDaddy <147562146+RaddyIsDaddy@users.noreply.github.com>
2026-02-16 21:18:28 -0500

    Started adding winning mechanics

--- Assets/calculateRewards.cs
+++ Assets/calculateRewards.cs
@@ -8,43 +8,40 @@ public class calculateRewards : MonoBehaviour
 
     public static Text winText;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static List<List<int>> rowIncludes = new List<List<int>>()
+    {
+        
+        new List<int>(){4, 7, 10, 13}, // 1
+        
+        new List<int>(){4, 5, 7, 8, 10, 11, 12, 13}, // 2
+        
+        new List<int>(){5, 6, 8, 9, 10, 11, 12, 13}, // 3
+        
+        new List<int>(){6, 9, 11, 12}, // 4
+        
+        new List<int>(){0, 1, 8, 10, 13}, // 5
+        
+        new List<int>(){1, 2, 7, 9, 11, 12, 13}, // 6
+        
+        new List<int>(){2, 3, 8, 9, 10, 13}, // 7
+        
+        new List<int>(){0, 1, 5, 11, 12}, // 8
+        
+        new List<int>(){1, 2, 4, 6, 10, 13}, // 9
+        
+        new List<int>(){2, 3, 5, 11, 12}, // 10
+        
+        new List<int>(){0, 1, 2, 4, 6, 8, 13}, // 11
+        
+        new List<int>(){1, 2, 3, 5, 7, 9, 12}, // 12
+        
+        new List<int>(){1, 2, 3, 5, 7, 9, 11}, // 13
+        
+        new List<int>(){0, 1, 2, 4, 6, 8, 10}, // 14
+    };
     public static List<List<Vector2Int>> lines = new List<List<Vector2Int>>()
     {
         // 1 FIRST ROW FROM BOTTOM
-        new List<Vector2Int>()
-        {
-            new Vector2Int(0, 0),
-            new Vector2Int(1, 0),
-            new Vector2Int(2, 0),
-            new Vector2Int(3, 0),
-            new Vector2Int(4, 0),
-            new Vector2Int(5, 0)
-        },
-
-        // 2 SECOND ROW FROM BOTTOM
-        new List<Vector2Int>()
-        {
-            new Vector2Int(0, 1),
-            new Vector2Int(1, 1),
-            new Vector2Int(2, 1),
-            new Vector2Int(3, 1),
-            new Vector2Int(4, 1),
-            new Vector2Int(5, 1)
-        },
-
-        // 3 THIRD ROW FROM BOTTOM
-        new List<Vector2Int>()
-        {
-            new Vector2Int(0, 2),
-            new Vector2Int(1, 2),
-            new Vector2Int(2, 2),
-            new Vector2Int(3, 2),
-            new Vector2Int(4, 2),
-            new Vector2Int(5, 2)
-        },
-
-        // 4 FOUTH ROW FROM BOTTOM
         new List<Vector2Int>()
         {
             new Vector2Int(0, 3),
@@ -52,7 +49,136 @@ public class calculateRewards : MonoBehaviour
             new Vector2Int(2, 3),
             new Vector2Int(3, 3),
             new Vector2Int(4, 3),
-            new Vector2Int(5, 3)
+        },
+
+        // 2 SECOND ROW FROM BOTTOM
+        new List<Vector2Int>()
+        {
+            new Vector2Int(0, 2),
+            new Vector2Int(1, 2),
+            new Vector2Int(2, 2),
+            new Vector2Int(3, 2),
+            new Vector2Int(4, 2),
+        },
+
+        // 3 THIRD ROW FROM BOTTOM
+        new List<Vector2Int>()
+        {
+            new Vector2Int(0, 1),
+            new Vector2Int(1, 1),
+            new Vector2Int(2, 1),
+            new Vector2Int(3, 1),
+            new Vector2Int(4, 1),
+        },
+
+        // 4 FOUTH ROW FROM BOTTOM
+        new List<Vector2Int>()
+        {
+            new Vector2Int(0, 0),
+            new Vector2Int(1, 0),
+            new Vector2Int(2, 0),
+            new Vector2Int(3, 0),
+            new Vector2Int(4, 0),
+        },
+
+        // 5
+        new List<Vector2Int>()
+        {
+            new Vector2Int(0, 3),
+            new Vector2Int(1, 2),
+            new Vector2Int(2, 3),
+            new Vector2Int(3, 2),
+            new Vector2Int(4, 3),
+        },
+
+        // 6
+        new List<Vector2Int>()
+        {
+            new Vector2Int(0, 2),
+            new Vector2Int(1, 1),
+            new Vector2Int(2, 2),
+            new Vector2Int(3, 1),
+            new Vector2Int(4, 2),
+        },
+
+        // 7
+        new List<Vector2Int>()
+        {
+            new Vector2Int(0, 1),
+            new Vector2Int(1, 0),
+            new Vector2Int(2, 1),
+            new Vector2Int(3, 0),
+            new Vector2Int(4, 1),
+        },
+
+        // 8
+        new List<Vector2Int>()
+        {
+            new Vector2Int(0, 2),
+            new Vector2Int(1, 3),
+            new Vector2Int(2, 2),
+            new Vector2Int(3, 3),
+            new Vector2Int(4, 2),
+        },
+
+        // 9
+        new List<Vector2Int>()
+        {
+            new Vector2Int(0, 1),
+            new Vector2Int(1, 2),
+            new Vector2Int(2, 1),
+            new Vector2Int(3, 2),
+            new Vector2Int(4, 1),
+        },
+
+        // 10
+        new List<Vector2Int>()
+        {
+            new Vector2Int(0, 0),
+            new Vector2Int(1, 1),
+            new Vector2Int(2, 0),
+            new Vector2Int(3, 1),
+            new Vector2Int(4, 0),
+        },
+
+        // 11
+        new List<Vector2Int>()
+        {
+            new Vector2Int(0, 3),
+            new Vector2Int(1, 2),
+            new Vector2Int(2, 1),
+            new Vector2Int(3, 2),
+            new Vector2Int(4, 3),
+        },
+
+        // 12
+        new List<Vector2Int>()
+        {
+            new Vector2Int(0, 2),
+            new Vector2Int(1, 1),
+            new Vector2Int(2, 0),
+            new Vector2Int(3, 1),
+            new Vector2Int(4, 2),
+        },
+
+        // 13
+        new List<Vector2Int>()
+        {
+            new Vector2Int(0, 0),
+            new Vector2Int(1, 1),
+            new Vector2Int(2, 2),
+            new Vector2Int(3, 1),
+            new Vector2Int(4, 0),
+        },
+
+        // 14
+        new List<Vector2Int>()
+        {
+            new Vector2Int(0, 1),
+            new Vector2Int(1, 2),
+            new Vector2Int(2, 3),
+            new Vector2Int(3, 2),
+            new Vector2Int(4, 1),
         },
     };
     void Start()
@@ -67,7 +193,6 @@ public class calculateRewards : MonoBehaviour
     
     public static void WinningLine(int line, int id)
     {
-
         List<Vector2Int> l1 = lines[line];
         int[,] symbolIDs = logicScript.symbolIDs;
 
@@ -75,5 +200,80 @@ public class calculateRewards : MonoBehaviour
         {
             symbolIDs[c.x, c.y] = id;
         }
+    }
+    public static int NumberOfWins()
+    {
+        float r = Random.Range(0, 100);
+        if (r <= 50)
+        {
+            return 1;
+        }
+        else if (r <= 75)
+        {
+            return 2;
+        }
+        else if (r <= 90)
+        {
+            return 3;
+        }
+        else
+        {
+            return 4;
+        }
+    }
+    public static List<double> WinMultiplier()
+    {
+        int item;
+        int amt;
+        double multi = 0;
+
+        float ri = Random.Range(0f, 100f);
+        if (ri < 40f)        item = 1;
+        else if (ri < 65f)   item = 2;
+        else if (ri < 80f)   item = 3;
+        else if (ri < 90f)   item = 4;
+        else if (ri < 97f)   item = 5;
+        else                item = 6;
+        
+        float ra = Random.Range(0f, 100f);
+        if (ra < 55f)        amt = 3;
+        else if (ra < 80f)   amt = 4;
+        else if (ra < 95f)   amt = 5;
+        else   amt = 6;
+
+        if (item == 1 && amt == 3) multi = 0.1;
+        else if (item == 1 && amt == 4) multi = 0.5;
+        else if (item == 1 && amt == 5) multi = 2;
+
+        if (item == 2 && amt == 3) multi = 0.1;
+        else if (item == 2 && amt == 4) multi = 0.5;
+        else if (item == 2 && amt == 5) multi = 2;
+
+        if (item == 3 && amt == 3) multi = 0.2;
+        else if (item == 3 && amt == 4) multi = 1;
+        else if (item == 3 && amt == 5) multi = 3;
+
+        if (item == 4 && amt == 3) multi = 0.2;
+        else if (item == 4 && amt == 4) multi = 1;
+        else if (item == 4 && amt == 5) multi = 3;
+
+        if (item == 5 && amt == 3) multi = 1;
+        else if (item == 5 && amt == 4) multi = 3;
+        else if (item == 5 && amt == 5) multi = 10;
+
+        if (item == 6 && amt == 3) multi = 1;
+        else if (item == 6 && amt == 4) multi = 3;
+        else if (item == 6 && amt == 5) multi = 10;
+
+        if (item == 7 && amt == 3) multi = 1.5;
+        else if (item == 7 && amt == 4) multi = 5;
+        else if (item == 7 && amt == 5) multi = 15;
+
+        if (item == 8 && amt == 3) multi = 2;
+        else if (item == 8 && amt == 4) multi = 7.5;
+        else if (item == 8 && amt == 5) multi = 20;
+
+        List<double> l = new List<double>() {item, amt, multi};
+        return l;
     }
 }
--- Assets/logicScript.cs
+++ Assets/logicScript.cs
@@ -3,7 +3,10 @@ using System.Collections;
 using System.Threading;
 using UnityEditor.Tilemaps;
 using UnityEngine;
+using System.Collections.Generic;
 using Random = UnityEngine.Random;
+using UnityEngine.UI;
+using System.Reflection.Emit;
 
 public class logicScript : MonoBehaviour
 {
@@ -13,9 +16,10 @@ public class logicScript : MonoBehaviour
     public GameObject d;
     public GameObject e;
     public GameObject f;
+    public Text winningText;
 
 
-    public static int xcol = 6;
+    public static int xcol = 5;
     public static int ycol = 4;
 
     public static GameObject[,] tiles;
@@ -61,14 +65,26 @@ public class logicScript : MonoBehaviour
         GameObject tile = tiles[x, y];
 
         int id = symbolIDs[x, y];
-        GameObject tilePrefab = a;
+        GameObject tilePrefab = f;
 
-        if (id == 0)
+        if (id == 1)
         {
             tilePrefab = a;
-        }else if (id == 1)
+        }else if (id == 2)
         {
             tilePrefab = b;
+        }else if (id == 3)
+        {
+            tilePrefab = c;
+        }else if (id == 4)
+        {
+            tilePrefab = d;
+        }else if (id == 5)
+        {
+            tilePrefab = e;
+        }else if (id == 6)
+        {
+            tilePrefab = f;
         }
 
         Vector3 startPos = new Vector3(x - offsetx, y - offsety + 5, 0);
@@ -78,7 +94,6 @@ public class logicScript : MonoBehaviour
         tile.transform.parent = transform;
         tile.name = "Slot Tile Specific";
         tiles[x, y] = tile;
-        Debug.Log("Spawning x: " + x.ToString() + " and y:" + y.ToString() + " At: " + startPos.ToString());
 
         teleportDownScript dropAnim = tile.AddComponent<teleportDownScript>();
         dropAnim.targetPosition = targetPos;
@@ -110,8 +125,56 @@ public class logicScript : MonoBehaviour
             tiles = new GameObject[xcol, ycol];
             symbolIDs = new int[xcol, ycol];
 
-            int line = Random.Range(0, 4);
-            calculateRewards.WinningLine(line, 1);
+            Dictionary<int, int> lineToLetter = new();
+            List<int> usedLine = new();
+
+            int winAmt = calculateRewards.NumberOfWins();
+            Debug.Log("Win Amount: " + winAmt);
+
+            for (int i = 0; i < winAmt; i++)
+            {
+                List<int> availableLines = new(){0,1,2,3,4,5,6,7,8,9,10,11,12,13};
+
+                List<double> stats = calculateRewards.WinMultiplier();
+                int item = (int) stats[0];
+                int amt = (int) stats[1];
+                double multi = stats[2];
+                int line = -1;
+                Debug.Log("Item:  " + item + " Amount: " + amt + " Multi: " + multi);
+
+                foreach (int l in usedLine)
+                {
+                    List<int> interceptLines = calculateRewards.rowIncludes[l];
+                    if (lineToLetter[l] == item)
+                    {
+                        foreach (int u in usedLine)
+                        {
+                            if (interceptLines.Contains(u))
+                            {
+                                interceptLines.Remove(u);
+                            }
+
+                        }
+                        line = interceptLines[Random.Range(0, interceptLines.Count)];
+                        Debug.Log(i + " Winning Line: " + line);
+                    }
+
+                }
+                if (line == -1)
+                {
+                    foreach (int u in usedLine)
+                        {
+                            if (availableLines.Contains(u))
+                            {
+                                availableLines.Remove(u);
+                            }
+
+                        }
+                }
+                
+                
+            }
+            
 
             StartCoroutine(spawnRowsWait());
             pressedSpin = false;
